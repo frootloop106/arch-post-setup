7d9c02916ccd904e85ae6674f96c86d77cfe643f
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-09-24 18:38:25 +0000

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --noconfirm plasma 
 sudo pacman -R --noconfirm plasma-sdk discover
-sudo pacman -S --noconfirm ark dolphin flameshot firefox kcalc konsole gedit
+sudo pacman -S --noconfirm ark dolphin flameshot firefox kcalc konsole gedit libreoffice-fresh
 sudo systemctl enable sddm
 sudo reboot 
