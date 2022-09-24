cebb119e72641538950f38c2d46bc1ca8d90a7b3
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-09-24 21:03:08 +0200

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -1,5 +1,6 @@
 sudo pacman -S --noconfirm plasma 
 sudo pacman -R --noconfirm plasma-sdk discover
 sudo pacman -S --noconfirm ark dolphin flameshot firefox kcalc konsole gedit libreoffice-fresh
+sudo pacman -S --noconfirm noto-fonts-emoji noto-fonts-cjk
 sudo systemctl enable sddm
 sudo reboot 
