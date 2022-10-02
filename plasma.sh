1ed0ce6d79e2cab97ad31696a452e35354d679ca
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-10-02 08:22:59 +0000

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -2,6 +2,6 @@ sudo pacman -S --noconfirm plasma
 sudo pacman -R --noconfirm plasma-sdk discover
 sudo pacman -S --noconfirm ark dolphin flameshot kcalc konsole kwrite libreoffice-fresh vlc
 sudo pacman -S --noconfirm noto-fonts-emoji noto-fonts-cjk
-paru -S brave-bin
 sudo systemctl enable sddm
+paru brave-bin
 sudo reboot 
