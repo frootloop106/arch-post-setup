5b6b1d907e38272e2fa4e1da3df6ec33f8e88693
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-10-22 06:43:08 +0000

    Delete drivers.sh

--- drivers.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S --noconfirm linux-headers
-sudo pacman -S --needed --noconfirm nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
-sudo pacman -S --needed --noconfirm lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
