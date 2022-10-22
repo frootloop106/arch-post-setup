20faa4b482b3507276443fedd89fca313219c180
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-10-22 06:43:00 +0000

    Delete plasma.sh

--- plasma.sh
+++
@@ -1,6 +0,0 @@
-sudo pacman -S --noconfirm plasma 
-sudo pacman -R --noconfirm plasma-sdk discover
-sudo pacman -S --noconfirm ark dolphin flameshot kcalc konsole kwrite libreoffice-fresh vlc firefox
-sudo pacman -S --noconfirm noto-fonts-emoji noto-fonts-cjk
-sudo systemctl enable sddm
-sudo reboot 
