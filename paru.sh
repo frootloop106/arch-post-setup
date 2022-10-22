b576a2f30f8336a901a1baececc88895a54107e0
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-10-22 06:42:29 +0000

    Add files via upload
    script to downloading the aur wrapper

---
+++ paru.sh
@@ -0,0 +1,16 @@
+#Downloading
+sudo pacman -S --needed --noconfirm base-devel git
+git clone https://aur.archlinux.org/paru.git
+cd paru
+# Installing paru
+makepkg -si --noconfirm
+cd
+cd arch-post-setup
+sudo rm -r paru
+clear
+# Installing Microsoft Fonts
+paru -S --noconfirm ttf-ms-fonts
+clear
+# Clearing cache
+paru -Sc
+
