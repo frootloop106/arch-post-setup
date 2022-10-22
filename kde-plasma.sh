d95b7c90f52c27cca2188a5ee7dd0ad4c1dcb243
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-10-22 06:47:32 +0000

    Add files via upload
    script for downloading the kde plasma desktop enviroment (with some changes)

---
+++ kde-plasma.sh
@@ -0,0 +1,15 @@
+# Installing the desktop
+sudo pacman -S --noconfirm plasma
+# Removing the unneeded packages
+sudo pacman -R plasma-sdk discover
+clear
+# Installing the packages
+sudo pacman -S --noconfirm ark dolphin flameshot kcalc konsole kate libreoffice-fresh vlc
+clear
+# Installing Firefox
+sudo pacman -S --nconfirm firefox
+clear
+# Enabling SDDM & clearing pacman's cache
+sudo systemctl enable sddm
+sudo pacman -Sc
+reboot
