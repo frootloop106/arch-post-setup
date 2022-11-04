97045eadb8d26f8e8fa4a118ad3591ee8e6f5e03
kuroeclipse <113916407+kuroeclipse@users.noreply.github.com>
2022-11-04 20:50:44 +0100

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,12 +1,12 @@
 # Checking Up to Date Packages
 pacman -Syyu --needed --noconfirm git base-devel
 # Installing the required file via git
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
+git clone https://aur.archlinux.org/paru.git
+cd paru
 # Instalation Time!
 makepkg -si --noconfirm
 clear
 # Installing MS FONTS
-yay -S --noconfirm ttf-ms-fonts
+paru -S --noconfirm ttf-ms-fonts
 echo "Instalation Complete!"
 
